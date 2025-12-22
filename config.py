6e7b4af9d274f92bd9971f472cc37efda15776d5
Sean Simpson <seansimpson@mac.lan>
2025-12-22 10:54:09 -0500

    Initial commit: Updated for CircuitPython 10 and WiFiManager stability

---
+++ config.py
@@ -0,0 +1,60 @@
+from adafruit_bitmap_font import bitmap_font
+
+config = {
+	#########################
+	# Network Configuration #
+	#########################
+
+	# WIFI Network SSID
+	'wifi_ssid': '<your ssid here>',
+
+	# WIFI Password
+	'wifi_password': '<your pasword>',
+
+	#########################
+	# Metro Configuration   #
+	#########################
+
+	# Metro Station Code
+	'metro_station_code': 'F04',
+
+	# Metro Train Groups
+    # If you only want 1 group to show up, use that code for both train_group_1 and train_group_2
+	'train_group_1': '1',
+    'train_group_2': '2',
+
+	# API Key for WMATA
+	'metro_api_key': '<your api key>',
+
+	#########################
+	# Other Values You      #
+	# Probably Shouldn't    #
+	# Touch                 #
+	#########################
+    'metro_api_url': 'http://api.wmata.com/StationPrediction.svc/json/GetPrediction/', # must use http instead of https for cert reasons
+	'metro_api_retries': 2,
+	'refresh_interval': 5, # 5 seconds is a good middle ground for updates, as the processor takes its sweet ol time
+	
+	# Display Settings
+	'matrix_width': 64,
+	'num_trains': 3,
+	'font': bitmap_font.load_font('lib/5x7.bdf'),
+
+	'character_width': 5,
+	'character_height': 7,
+	'text_padding': 1,
+	'text_color': 0xFF7500,
+
+	'loading_destination_text': 'Loading',
+	'loading_min_text': '---',
+	'loading_line_color': 0xFF00FF, # Something something Purple Line joke
+
+	'heading_text': 'LN DEST   MIN',
+	'heading_color': 0xFF0000,
+
+	'train_line_height': 6,
+	'train_line_width': 2,
+
+	'min_label_characters': 3,
+	'destination_max_characters': 9,
+}
---
+++ metro_api.py
@@ -0,0 +1,83 @@
+import board
+import busio
+import time
+import neopixel
+from digitalio import DigitalInOut
+from adafruit_esp32spi import adafruit_esp32spi
+from adafruit_esp32spi.adafruit_esp32spi_wifimanager import WiFiManager
+from config import config
+from secrets import secrets
+
+# Setup the raw hardware pins
+esp32_cs = DigitalInOut(board.ESP_CS)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+# Initialize the onboard NeoPixel
+status_pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.1)
+
+# Pass it to the WiFiManager
+wifi = WiFiManager(esp, secrets["ssid"], secrets["password"], status_pixel=status_pixel)
+
+class MetroApiOnFireException(Exception):
+    pass
+
+class MetroApi:
+    @staticmethod
+    def fetch_train_predictions(station_code: str, group: str) -> list:
+        api_url = config['metro_api_url'] + station_code
+        
+        try:
+            # wifi.get() automatically checks if the chip is alive and resets it if not!
+            response = wifi.get(
+                api_url, 
+                headers={'api_key': config['metro_api_key']},
+                timeout=15
+            )
+            
+            with response: # This ensures the socket is closed automatically
+                train_data = response.json()
+                print('Received response from WMATA api...')
+                trains = filter(lambda t: t['Group'] == group, train_data['Trains'])
+                return list(map(MetroApi._normalize_train_response, trains))
+
+        except Exception as e:
+            print(f"WiFiManager caught an error: {e}")
+            # If WiFiManager couldn't fix it after its internal retries:
+            raise MetroApiOnFireException()
+
+    @staticmethod
+    def _normalize_train_response(train: dict) -> dict:
+        line = train.get('Line', '--')
+        destination = train.get('Destination', 'Unknown')
+        arrival = train.get('Min', '--')
+
+        # Clean up long destination names for the matrix
+        map_dest = {
+            'No Passenger': 'No Psngr',
+            'NoPssenger': 'No Psngr',
+            'ssenger': 'No Psngr',
+            'Greenbelt': 'Grnbelt',
+            'Branch Av': 'Brnch Av'
+        }
+        destination = map_dest.get(destination, destination)
+
+        return {
+            'line_color': MetroApi._get_line_color(line),
+            'destination': destination,
+            'arrival': arrival
+        }
+
+    @staticmethod
+    def _get_line_color(line: str) -> int:
+        colors = {
+            'RD': 0xFF0000,
+            'OR': 0xFF5500,
+            'YL': 0xFFFF00,
+            'GR': 0x00FF00,
+            'BL': 0x0000FF,
+            'SV': 0xAAAAAA # Silver Line
+        }
+        return colors.get(line, 0x444444)
